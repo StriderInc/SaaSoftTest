6679b23ff2a8a5e5bc10260dcbc3167cde2dc87d
Gleb <korotkiygs@yandex.ru>
2025-02-25 10:40:52 +0300

    add: fill fieldConfig

--- src/pages/account-manager/config/fieldConfig.ts
+++ src/pages/account-manager/config/fieldConfig.ts
@@ -1,6 +1,21 @@
 export const fieldConfig = {
-  tag: {},
-  recordType: {},
-  login: {},
-  password: {},
+  tag: {
+    component: "customInput",
+    hideLabel: true,
+  },
+  recordType: {
+    component: "select",
+    hideLabel: true,
+    inputProps: {
+      selectValueClass: "min-w-[100px]",
+    },
+  },
+  login: {
+    component: "customInput",
+    hideLabel: true,
+  },
+  password: {
+    component: "password",
+    hideLabel: true,
+  },
 };
