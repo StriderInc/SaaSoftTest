85d1bef3f3592eaa977c8c83099d4c8d70741342
Gleb <korotkiygs@yandex.ru>
2025-02-26 08:01:42 +0300

    add: tooltip tag text

--- src/pages/account-manager/ui/AccountHeader.vue
+++ src/pages/account-manager/ui/AccountHeader.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AccountTagTooltip from "./AccountTagTooltip.vue";
+</script>
 
 <template>
   <div class="flex items-center justify-center">
     <div class="grid grid-cols-4 grid-rows-[auto_1fr] gap-[5px] w-[50%]">
-      <span>Метки</span>
+      <div class="flex items-center justify-start gap-3">
+        <span>Метки</span>
+        <AccountTagTooltip />
+      </div>
       <span>Тип записи</span>
       <span>Логин</span>
       <span>Пароль</span>
---
+++ src/pages/account-manager/ui/AccountTagTooltip.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@shared/ui/shadcn/tooltip";
+import { CircleHelp } from "lucide-vue-next";
+</script>
+
+<template>
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger class="cursor-pointer">
+        <CircleHelp />
+      </TooltipTrigger>
+      <TooltipContent class="bg-blue-300">
+        <p>
+          Для указания нескольких меток одной пары логин/пароль используйте
+          разделитель ;
+        </p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+</template>
